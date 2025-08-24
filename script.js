9bbd0d52ef5ed91fb5eb704613585afd863ebfbe
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-08-24 17:34:22 +0600

    developed shared funtions

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById("loginButton").addEventListener("click", (e) => {
   const pinNumberConverted = parseInt(pinNumber);
 
   if (actualMobileNumber === mobileNumberConverted && actualPinNumber === pinNumberConverted){
-    window.location.href = "/PH-JS-Payoo-Mobile-App/home.html";
+    window.location.href = "./home.html";
   } else {
     alert("Invalid Credentials");
   }
