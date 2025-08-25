7b05daaf0ca8f904c1f3c6a17ec06fa21a0988d9
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-08-25 15:35:37 +0600

    transfer money funtionality added

--- home.js
+++ home.js
@@ -1,19 +1,27 @@
-// shared functions 
+// shared functions
 
-function getIntValue (id) {
+function getIntValue(id) {
   return parseInt(document.getElementById(id).value);
 }
 
-function getIntInnerText (id) {
+function getIntInnerText(id) {
   return parseInt(document.getElementById(id).innerText);
 }
 
-function getStrValue (id) {
+function getStrValue(id) {
   return document.getElementById(id).value;
 }
 
+function handleToggle(id) {
+  const forms = document.getElementsByClassName("form");
+  for (const form of forms) {
+    form.style.display = "none";
+  }
 
-// add button 
+  document.getElementById(id).style.display = "block";
+}
+
+// add button
 document.getElementById("btn-add-money").addEventListener("click", (e) => {
   e.preventDefault();
   const addBank = getStrValue("add-bank");
@@ -27,24 +35,33 @@ document.getElementById("btn-add-money").addEventListener("click", (e) => {
   document.getElementById("available-amount").innerText = updatedAmount;
 });
 
-// cash out button 
+// cash out button
 document.getElementById("btn-cash-out").addEventListener("click", (e) => {
   e.preventDefault();
   const cashOutAmount = getIntValue("cash-out-amount");
   const availableAmount = getIntInnerText("available-amount");
-  document.getElementById("available-amount").innerText = availableAmount - cashOutAmount;
+  document.getElementById("available-amount").innerText =
+    availableAmount - cashOutAmount;
+});
+
+//transfer money button
+document.getElementById("btn-transfer-money").addEventListener("click", (e) => {
+  e.preventDefault();
+  const amount = getIntValue("transfer-money-amount");
+  const availableAmount = getIntInnerText("available-amount")
+  document.getElementById("available-amount").innerText = availableAmount - amount;
 })
 
 // implementing toggle feature
 
 document.getElementById("add-money-button").addEventListener("click", (e) => {
-  document.getElementById("add-money-section").style.display = "block";
-  document.getElementById("cash-out-section").style.display = "none";
+  handleToggle("add-money-section");
 });
 
-document
-  .getElementById("cashOut-money-button")
-  .addEventListener("click", (e) => {
-    document.getElementById("add-money-section").style.display = "none";
-    document.getElementById("cash-out-section").style.display = "block";
-  });
+document.getElementById("cashOut-money-button").addEventListener("click", (e) => {
+  handleToggle("cash-out-section");
+});
+
+document.getElementById("transfer-money-button").addEventListener("click", (e) => {
+  handleToggle("transfer-money-section");
+})
