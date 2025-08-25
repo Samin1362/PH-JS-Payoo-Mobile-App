662c6dd2bc8d72c3e2caf644ae67daa99a2a8c7b
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-08-25 20:08:26 +0600

    transaction feature added

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
-// shared functions
+const transactions = [];
 
+// shared functions
 function getIntValue(id) {
   return parseInt(document.getElementById(id).value);
 }
@@ -24,7 +25,7 @@ function handleToggle(id) {
 function handleToggleButton(id) {
   const cardButtons = document.getElementsByClassName("card-button");
 
-  for (const cardButton of cardButtons){
+  for (const cardButton of cardButtons) {
     cardButton.classList.remove("border-blue-600", "bg-[#F4F5F7]");
     cardButton.classList.add("border-gray-300", "bg-white");
   }
@@ -45,6 +46,13 @@ document.getElementById("btn-add-money").addEventListener("click", (e) => {
   const updatedAmount = availableAmount + addAmount;
 
   document.getElementById("available-amount").innerText = updatedAmount;
+
+  //code for transaction history
+  const transactionHistory = {
+    transactionType: "Add Money",
+    date: new Date().toLocaleTimeString(),
+  };
+  transactions.push(transactionHistory);
 });
 
 // cash out button
@@ -72,14 +80,42 @@ document.getElementById("add-money-button").addEventListener("click", (e) => {
   handleToggleButton("add-money-button");
 });
 
-document
-  .getElementById("cashOut-money-button").addEventListener("click", (e) => {
+document.getElementById("cashOut-money-button").addEventListener("click", (e) => {
     handleToggle("cash-out-section");
     handleToggleButton("cashOut-money-button");
   });
 
-document
-  .getElementById("transfer-money-button").addEventListener("click", (e) => {
+document.getElementById("transfer-money-button").addEventListener("click", (e) => {
     handleToggle("transfer-money-section");
     handleToggleButton("transfer-money-button");
   });
+
+//transaction button
+document.getElementById("transation-button").addEventListener("click", () => {
+  handleToggle("transation-history-section");
+  handleToggleButton("transation-button");
+
+  //adding transactions
+  const transactionCards = document.getElementById("transaction-cards");
+  transactionCards.innerHTML = "";
+  
+  for (const transaction of transactions) {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+          <div class="transaction-card bg-white p-4 flex justify-between items-center rounded-lg mt-4">
+            <div class="flex items-center gap-2">
+              <div class="bg-[#F4F5F7] p-1 rounded-full"><img class="w-6" src="./assets/wallet1.png" alt=""></div>
+              <div>
+                <h1 class="font-bold">${transaction.transactionType}</h1>
+                <p>${transaction.date}</p>
+              </div>
+            </div>
+            <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+          </div>
+    `;
+
+    transactionCards.appendChild(div);
+  }
+  console.log(transactions);
+});
